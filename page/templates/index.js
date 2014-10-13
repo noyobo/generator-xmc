49d259ba79ffd7bcbace9fb8a3e3eada5e36a0a2
宝码 <nongyoubao@alibaba-inc.com>
2014-10-13 18:44:05 +0800

    v1.1.1 xtpl 增加包名

--- page/templates/index.js
+++ page/templates/index.js
@@ -6,7 +6,7 @@ var a = require('./mods/a');
 var hello = require('./views/hello-render')
 
 module.exports = function() {
-    var html = hello({name:'noyobo'})
+    var html = hello({name:'world'})
     alert(html)
     a.init();
     if ('@DEBUG@') {
