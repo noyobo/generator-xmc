03b3b99c5665f9acd96ba688d37cb1d8031bd4af
宝码 <nongyoubao@alibaba-inc.com>
2014-10-20 11:20:47 +0800

    加上无奈的提示

--- app/templates/index.js
+++ app/templates/index.js
@@ -4,12 +4,18 @@
 
 var a = require('./mods/a');
 var hello = require('./views/hello-render')
+// 在你删除此行代码的时候, 请记住 require views 文件的时候一定更要加上  -render !!!
+// @see https://github.com/kissyteam/xtemplate
 
 module.exports = function() {
-    var html = hello({name:'noyobo'})
+    var html = hello({name:'world'})
     alert(html)
     a.init();
     if ('@DEBUG@') {
       console.log('when @DEBUG@ show log');
     };
 }
+
+// 在你删除此代码块的时候, 请记住
+// 代码书写风格遵循 commonJS 规范
+// @see http://wiki.commonjs.org/wiki/Modules/1.0
--- page/templates/index.js
+++ page/templates/index.js
@@ -4,6 +4,8 @@
 
 var a = require('./mods/a');
 var hello = require('./views/hello-render')
+// 在你删除此行代码的时候, 请记住 require views 文件的时候一定更要加上  -render !!!
+// @see https://github.com/kissyteam/xtemplate
 
 module.exports = function() {
     var html = hello({name:'world'})
@@ -13,3 +15,6 @@ module.exports = function() {
       console.log('when @DEBUG@ show log');
     };
 }
+// 在你删除此代码块的时候, 请记住
+// 代码书写风格遵循 commonJS 规范
+// @see http://wiki.commonjs.org/wiki/Modules/1.0
